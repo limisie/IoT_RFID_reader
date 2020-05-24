0713a7a84eec3f937a0baa44e16ccd6fc27e24df
limisie <k.limisiewicz@gmail.com>
2020-05-24 19:49:11 +0200

    Messy commit 24.05.2020

---
+++ rfid_reader/content.py
@@ -0,0 +1,27 @@
+class Worker:
+    __worker_counter = 0
+
+    def __init__(self, name, surname, rdif_id):
+        self.__id = self.__worker_counter
+        self.__worker_counter += 1
+        self.__name = name
+        self.__surname = surname
+        self.__rdif_id = rdif_id
+        self.__logged_in = False
+        self.__logs = []
+
+    def get_full_name(self):
+        return str(self.__name, self.__surname)
+
+
+class Client:
+    __terminal_counter = 0
+
+    def __init__(self, name):
+        self.__terminal_id = self.__terminal_counter
+        self.__terminal_counter += 1
+        self.__name = name
+
+    def log_worker(self, rdif_id):
+        pass
+
